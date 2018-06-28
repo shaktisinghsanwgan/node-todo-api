ecb84508b2e4fd5daa0d7cc9968f66dcb4b052f8
shakti <shaktisinghsangwan@gmail.com>
2018-06-28 18:00:02 +0530

    todo by id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID}=require('mongodb');
+
 var express=require('express');
 
 var bodyParser=require('body-parser');
@@ -31,6 +33,27 @@ app.get('/todos',(req,res)=>{
 		res.send({todos});
 	},(e)=>{
 		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id',(req,res)=>{
+
+	var id=req.params.id;
+	
+	if(!ObjectID.isValid(id)){
+		res.status(404).send();
+	}
+	// finding the todo by id
+
+	Todo.findById(id).then((todo)=>{
+		if(!todo){
+			res.status(404).send();
+		}
+		res.send({todo});
+	},(e)=>{
+		res.status(404).send();
+	}).catch((e)=>{
+		res.status(400).send();
 	});
 });
 
